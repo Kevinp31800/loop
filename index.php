037009c8116b809f417c40bd7cc38be878a78627
kevin <kevinpuyol@live.fr>
2017-03-20 11:54:56 +0100

    exo 1

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>loop</title>
+</head>
+<body>
+	<h1><?php echo("PHP is awesome") ?></h1>
+	<?php
+	for ($i = 0; $i < 10 ; $i++) {
+		
+		echo $i;
+	}
+	?>
+</body>
+</html>
